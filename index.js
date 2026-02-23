3df6153f0ebfb99107f5108ef0446d22e29085eb
JnJsCortVas23 <juan.cortes87@correo.tdea.edu.co>
2026-02-23 17:38:29 -0500

    ObtenerEstado arreglado

--- index.js
+++ index.js
@@ -17,20 +17,24 @@ function Promedio(grades) {
     return suma/grades.length;   
 }//Version optimizada
 
-
-
 const ObtenerEstado = (promedio) => {
-    if(promedio <=2.9) return "Reprobado"
-    //else if((promedio > 3 ) && (promedio <=4)) return "Aceptable"
-    else return "Excelente"
+    if (promedio < 3) return "Reprobado";
+    else if (promedio <= 4) return "Aceptable";
+    else return "Excelente";
 };
+
+/*const ObtenerEstado = (promedio) => {
+    if(promedio < 3) return "Reprobado"
+    else if((promedio >= 3.1 ) && (promedio <= 4)) return "Aceptable"
+    else return "Excelente"
+};*/
 
 const mostrarReporte = (estudiante) => {
     const promedio = Promedio(estudiante.calificaciones);
     const estado = ObtenerEstado(promedio);
 
     console.log(Saludo(estudiante.nombre));
-    console.log((`Asignatura: ${estudiante.asigantura}`));
+    console.log((`Asignatura: ${estudiante.asignatura}`));
     console.log((`Promedio: ${promedio}`));
     console.log((`Estado: ${estado}`));
     console.log((`//////////////////`));
@@ -38,11 +42,11 @@ const mostrarReporte = (estudiante) => {
 }
 
 const estudiante = [
-    {nombre: "pepe", calificaciones:[1,2,3,4,5], asigantura: "calculo"},
-    {nombre: "toño", calificaciones:[5,4,3,2,1], asigantura: "web"},
-    {nombre: "Muñoz", calificaciones:[5,4,0,2,1], asigantura: "movil"},
-    {nombre: "Michi", calificaciones:[5,4,5,3,4], asigantura: "pruebas"},
-    {nombre: "Jose", calificaciones:[3,4,3,2,3], asigantura: "despliegue"}
+    {nombre: "pepe", calificaciones:[1,2,3,4,5], asignatura: "calculo"},
+    {nombre: "toño", calificaciones:[5,4,3,2,1], asignatura: "web"},
+    {nombre: "Muñoz", calificaciones:[5,4,0,2,1], asignatura: "movil"},
+    {nombre: "Michi", calificaciones:[5,4,5,3,4], asignatura: "pruebas"},
+    {nombre: "Jose", calificaciones:[3,4,3,2,3], asignatura: "despliegue"}
         ];
 
         estudiante.forEach(mostrarReporte);
